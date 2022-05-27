2aaf55cb2dbeb41caa767911003e4796ff8be94c
anameb <anameb92@gmail.com>
2022-05-27 00:33:53 -0500

    se da solucion a los ejercicio 1,2,4 y 5.

--- src/main/java/io/pivotal/literx/Part04Transform.java
+++ src/main/java/io/pivotal/literx/Part04Transform.java
@@ -15,21 +15,21 @@ public class Part04Transform {
 
 	// TODO Capitalize the user username, firstname and lastname
 	Mono<User> capitalizeOne(Mono<User> mono) {
-		return null;
+		return  mono.map(username -> new User(username.getUsername().toUpperCase(), username.getFirstname().toUpperCase(), username.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName
-	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+	Flux<User> capitalizeMany(Flux<User> Flux) {
+		return Flux.map(username -> new User(username.getUsername().toUpperCase(), username.getFirstname().toUpperCase(), username.getLastname().toUpperCase()));
 	}
 
 //========================================================================================
 
 	// TODO Capitalize the users username, firstName and lastName using #asyncCapitalizeUser
-	Flux<User> asyncCapitalizeMany(Flux<User> flux) {
-		return null;
+	Flux<User> asyncCapitalizeMany(Flux<User> Flux) {
+		return Flux.flatMap(username -> asyncCapitalizeUser(username));
 	}
 
 	Mono<User> asyncCapitalizeUser(User u) {
